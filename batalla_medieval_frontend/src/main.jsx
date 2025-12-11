1ab802edc1f83592dd85235e962dc9a9c1de6287
SoftMobile <12luisdubon@gmail.com>
2025-12-11 12:20:07 -0600

    Add intro animation and loading sequence

--- batalla_medieval_frontend/src/main.jsx
+++ batalla_medieval_frontend/src/main.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import LoadingScreen from './components/LoadingScreen.jsx';
+import IntroAnimation from './components/IntroAnimation.jsx';
 import './index.css';
+
+const ExperienceShell = () => {
+  const [introFinished, setIntroFinished] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
+
+  const handleIntroComplete = () => {
+    setIntroFinished(true);
+    setShowLoading(true);
+  };
+
+  const handleLoadingComplete = () => {
+    setShowLoading(false);
+  };
+
+  return (
+    <>
+      {!introFinished && <IntroAnimation onComplete={handleIntroComplete} />}
+      {showLoading && <LoadingScreen onComplete={handleLoadingComplete} />}
+      <App />
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ExperienceShell />
     </BrowserRouter>
   </React.StrictMode>
 );
